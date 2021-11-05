d3d7f5af6dab7845efe516244e297afc02a6c692
jrjturnbull <s1639556@ed.ac.uk>
2021-11-05 16:03:10 +0000

    deleted old shell files

--- run_combined.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 generate_combined.py "CHORUSNBPb_dw_ite" "CHORUSNUPb_dw_ite" "DYE605_dw_ite" "NTVNBDMNFe_dw_ite" "NTVNUDMNFe_dw_ite"
-
--- run_single.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 covariance.py "CHORUSNBPb_dw_ite"
-python3 covariance.py "CHORUSNUPb_dw_ite"
-python3 covariance.py "DYE605_dw_ite"
-python3 covariance.py "NTVNBDMNFe_dw_ite"
-python3 covariance.py "NTVNUDMNFe_dw_ite"
