767b8cc1f25ccca0456157d903f344ad8dea667d
jrjturnbull <93486953+jrjturnbull@users.noreply.github.com>
2021-11-07 13:52:16 +0000

    Create run_dw_ite.sh

---
+++ run_dw_ite.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# RUNS THE ENTIRE PROJECT (SO FAR) FOR THE DEWEIGHTED ITERATED DATA FILES
+# REQUIRES DATA, SYSTYPE, THEORY FILES FOR EACH EXPERIMENT
+
+python3 covariance.py "CHORUSNBPb_dw_ite"
+python3 covariance.py "CHORUSNUPb_dw_ite"
+python3 covariance.py "DYE605_dw_ite"
+python3 covariance.py "NTVNBDMNFe_dw_ite"
+python3 covariance.py "NTVNUDMNFe_dw_ite"
+
+python3 generate_combined.py "CHORUSNBPb_dw_ite" "CHORUSNUPb_dw_ite" "DYE605_dw_ite" "NTVNBDMNFe_dw_ite" "NTVNUDMNFe_dw_ite" "CombinedData_dw_ite"
+
+python3 covariance.py "CombinedData_dw_ite"
