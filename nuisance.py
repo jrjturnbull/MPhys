406792c05a89f2a081876b29b1e114cb1bcc7a9f
jrjturnbull <s1639556@ed.ac.uk>
2021-11-23 16:37:29 +0000

    clean nuisance.py

--- nuisance.py
+++ nuisance.py
@@ -8,14 +8,10 @@ from numpy.core.fromnumeric import shape
 root = sys.argv[1]
 
 th_covariance_matrix = np.load("matrices/CV_" + root + ".dat", allow_pickle=True)
-covariance_matrix_norm = np.load("matrices/CVN_" + root + ".dat", allow_pickle=True)
-correlation_matrix = np.load("matrices/CR_" + root + ".dat", allow_pickle=True)
 nuclear_uncertainty_array = np.load("matrices/NUA_" + root + ".dat", allow_pickle=True)
 exp_data = np.load("matrices/EXP_" + root + ".dat", allow_pickle=True)
-exp_covariance_matrix = np.load("matrices/ECV_" + root + ".dat", allow_pickle=True)
 theory_data = np.load("matrices/TH_" + root + ".dat", allow_pickle=True)
 eigenvectors = np.load("matrices/EVC_" + root + ".dat", allow_pickle=True)
-eigenvectors_norm = np.load("matrices/EVCN_" + root + ".dat", allow_pickle=True)
 
 n_nuis = shape(nuclear_uncertainty_array)[1]
 
@@ -23,7 +19,7 @@ nuisance_params = np.zeros(shape=n_nuis, dtype=float64)
 
 for alpha in range(0, len(eigenvectors)):
     beta = nuclear_uncertainty_array[:, alpha]
-    beta = eigenvectors_norm[alpha]
+    beta = eigenvectors[alpha]
     CS = inv(th_covariance_matrix)
     mat = np.matmul(beta, CS)
     TD = theory_data - exp_data
