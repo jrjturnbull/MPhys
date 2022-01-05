a2438e909eeae6f8a20de89fa324d12b57625cef
Jamie Turnbull <s1639556@ed.ac.uk>
2022-01-05 14:52:56 +0000

    autopredictions

--- autoprediction.py
+++ autoprediction.py
@@ -13,12 +13,16 @@ x_matrix = np.load("matrices/XCV_" + root + ".dat", allow_pickle=True)
 
 CS = inv(exp_covariance_matrix + th_covariance_matrix)
 
+
+
 # DETERMINE AUTOPREDICTION SHIFTS
 SCS = np.einsum('ij,jk->ik', th_covariance_matrix, CS)
 TD = theory_data - exp_data
 delta_T = -np.einsum('ij,j->i', SCS, TD)
 delta_T_norm = np.divide(delta_T, theory_data) # the delta T_i shifts
 TD_diff = -np.divide(TD, theory_data) # the differences between theory and data D_i - T_i
+
+
 
 # DETERMINE AUTOPREDICTION COVARIANCE AND CORRELATION MATRICES
 term_1 = np.einsum('ij,jk,kl,lm,mn->in', exp_covariance_matrix, CS, x_matrix, CS, exp_covariance_matrix, optimize='optimal')
@@ -31,3 +35,12 @@ for i in range(len(autoprediction)):
             autoprediction_corr[i,j] = autoprediction[i,j] / sqrt(autoprediction[i,i] * autoprediction[j,j])
 
 autoprediction_cons = x_matrix + th_covariance_matrix
+
+
+
+# DETERMINE CONTRIBUTIONS TO THE DIAGONAL ELEMENTS OF THE CORRELATED THEORY & PDF UNCERTAINTIES
+th_contribution_1 = th_covariance_matrix - np.einsum('ij,jk,kl->il', th_covariance_matrix,CS, th_covariance_matrix)
+th_contribution_2 = th_contribution_1 + np.einsum('ij,jk,kl,lm,mn->in', th_covariance_matrix, CS, x_matrix, CS, th_covariance_matrix)
+
+x_contribution_1 = np.einsum('ij,jk,kl,lm,mn->in', exp_covariance_matrix, CS, x_matrix, CS, exp_covariance_matrix)
+x_contribution_2 = x_matrix - np.einsum('ij,jk,kl->il', th_covariance_matrix,CS,x_matrix) - np.einsum('ij,jk,kl->il', x_matrix,CS,th_covariance_matrix)
